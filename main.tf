a186c41fbe4c7f96f9da815230814797448e8912
dnewmanPHOENIX <63229615+dnewmanPHOENIX@users.noreply.github.com>
2026-01-06 15:46:05 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "5dce397f-146b-42af-8708-e37fa06b79d7"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatedan260106"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
